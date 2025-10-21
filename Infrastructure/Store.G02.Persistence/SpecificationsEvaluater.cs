41b57537bd28a98273d9ad7a04cb71318be2e0d4
sohila558 <sohailat.441@gmail.com>
2025-10-21 22:08:41 +0300

    Prepare Specification Class

---
+++ Infrastructure/Store.G02.Persistence/SpecificationsEvaluater.cs
@@ -0,0 +1,18 @@
+﻿using Store.G05.Domain.Contracts;
+using Store.G05.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Persistence
+{
+    public static class SpecificationsEvaluater<TEntity>
+    {
+        public static IQueryable<TEntity> GetQuery<TEntity, TKey>(ISpecefications<TEntity, TKey> spec) where TEntity : BaseEntity<TKey>
+        {
+
+        }
+    }
+}
