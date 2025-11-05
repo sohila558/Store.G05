3e80486afea61e73ea45517c3c4bdcd2eca11e68
sohila558 <sohailat.441@gmail.com>
2025-11-05 23:18:48 +0200

    Order Profile

---
+++ Core/Store.G05.Services/Mapping/OrderProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Store.G02.Shared.Dtos.Orders;
+using Store.G05.Domain.Entities.Orders;
+using Store.G05.Services.Abstractions.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G05.Services.Mapping
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<OrderAddress, OrderAddressDto>().ReverseMap();
+            
+            CreateMap<Order, OrderResponse>()
+                .ForMember(D => D.DeliveryMethod, O => O.MapFrom(S => S.DeliveryMethod.ShortName))
+                .ForMember(D => D.Total, O => O.MapFrom(S => S.GetTotal()));
+            
+            CreateMap<OrderItem, OrderItemDto>()
+                .ForMember(D => D.ProductId, O => O.MapFrom(S => S.Product.ProductId))
+                .ForMember(D => D.ProductName, O => O.MapFrom(S => S.Product.ProductName))
+                .ForMember(D => D.PictureUrl, O => O.MapFrom(S => S.Product.PictureUrl));
+        }
+    }
+}
